afa34de2a18d448cbcc7675291e064367f2ecd7a
margaret-ngugi <ngugiwairimumargaret@gmail.com>
2023-05-03 19:31:47 +0300

    first commit

--- app/src/main/java/com/example/mycalculator1/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator1/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         addNumbers()
         multiplication()
+        division()
+        subtraction()
     }
 
     fun addNumbers() {
@@ -38,18 +40,31 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "$result", Toast.LENGTH_SHORT).show()
         }
     }
-        fun division() {
-            var num1 = findViewById<EditText>(R.id.Number1)
-            var num2 = findViewById<EditText>(R.id.Number2)
-            var button2 = findViewById<Button>(R.id.btn_mul)
-            button2.setOnClickListener {
-                var addNum1 = num1.text.toString().toInt()
-                var addNum2 = num2.text.toString().toInt()
-                var result = addNum1 / addNum2
-                Toast.makeText(this, "$result", Toast.LENGTH_SHORT).show()
-            }
+
+    fun division() {
+        var num1 = findViewById<EditText>(R.id.Number1)
+        var num2 = findViewById<EditText>(R.id.Number2)
+        var button2= findViewById<Button>(R.id.btn_mul)
+        button2.setOnClickListener {
+            var addNum1 = num1.text.toString().toInt()
+            var addNum2 = num2.text.toString().toInt()
+            var result = addNum1 / addNum2
+            Toast.makeText(this, "$result", Toast.LENGTH_SHORT).show()
         }
     }
+
+    fun subtraction() {
+        var num1 = findViewById<EditText>(R.id.Number1)
+        var num2 = findViewById<EditText>(R.id.Number2)
+        var button3 = findViewById<Button>(R.id.btn_mul)
+        button3.setOnClickListener {
+            var addNum1 = num1.text.toString().toInt()
+            var addNum2 = num2.text.toString().toInt()
+            var result = addNum1 - addNum2
+            Toast.makeText(this, "$result", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
 
 
 
