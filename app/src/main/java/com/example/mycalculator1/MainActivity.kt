9e4b31f4b8cca198b75240b5a956e9781f242fbb
margaret-ngugi <ngugiwairimumargaret@gmail.com>
2023-05-07 23:16:51 +0300

    first commit

--- app/src/main/java/com/example/mycalculator1/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator1/MainActivity.kt
@@ -25,45 +25,46 @@ class MainActivity : AppCompatActivity() {
             var addNum1 = num1.text.toString().toInt()
             var addNum2 = num2.text.toString().toInt()
             var result = addNum1 + addNum2
-            Toast.makeText(this, "$result", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "$result", Toast.LENGTH_LONG).show()
+        }
+    }
+    fun subtraction() {
+        var num1 = findViewById<EditText>(R.id.Number1)
+        var num2 = findViewById<EditText>(R.id.Number2)
+        var button = findViewById<Button>(R.id.btn_sub)
+        button.setOnClickListener {
+            var subNum1 = num1.text.toString().toInt()
+            var subNum2 = num2.text.toString().toInt()
+            var result = subNum1 - subNum2
+            Toast.makeText(this, "$result", Toast.LENGTH_LONG).show()
         }
     }
 
     fun multiplication() {
         var num1 = findViewById<EditText>(R.id.Number1)
         var num2 = findViewById<EditText>(R.id.Number2)
-        var button1 = findViewById<Button>(R.id.btn_mul)
-        button1.setOnClickListener {
-            var addNum1 = num1.text.toString().toInt()
-            var addNum2 = num2.text.toString().toInt()
-            var result = addNum1 * addNum2
-            Toast.makeText(this, "$result", Toast.LENGTH_SHORT).show()
+        var button= findViewById<Button>(R.id.btn_mul)
+        button.setOnClickListener {
+            var mulNum1 = num1.text.toString().toInt()
+            var mulNum2 = num2.text.toString().toInt()
+            var result = mulNum1 * mulNum2
+            Toast.makeText(this, "$result", Toast.LENGTH_LONG).show()
         }
     }
 
     fun division() {
         var num1 = findViewById<EditText>(R.id.Number1)
         var num2 = findViewById<EditText>(R.id.Number2)
-        var button2= findViewById<Button>(R.id.btn_mul)
-        button2.setOnClickListener {
-            var addNum1 = num1.text.toString().toInt()
-            var addNum2 = num2.text.toString().toInt()
-            var result = addNum1 / addNum2
-            Toast.makeText(this, "$result", Toast.LENGTH_SHORT).show()
+        var button= findViewById<Button>(R.id.btn_div)
+        button.setOnClickListener {
+            var divNum1 = num1.text.toString().toInt()
+            var divNum2 = num2.text.toString().toInt()
+            var result = divNum1 / divNum2
+            Toast.makeText(this, "$result", Toast.LENGTH_LONG).show()
         }
     }
 
-    fun subtraction() {
-        var num1 = findViewById<EditText>(R.id.Number1)
-        var num2 = findViewById<EditText>(R.id.Number2)
-        var button3 = findViewById<Button>(R.id.btn_mul)
-        button3.setOnClickListener {
-            var addNum1 = num1.text.toString().toInt()
-            var addNum2 = num2.text.toString().toInt()
-            var result = addNum1 - addNum2
-            Toast.makeText(this, "$result", Toast.LENGTH_SHORT).show()
-        }
-    }
+
 }
 
 
